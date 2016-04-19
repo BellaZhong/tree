fd1803be48056cfa275ad50f3d6cd61d7a7fdeb2
Bella <906772411@qq.com>
2016-04-19 10:27:56 +0800

    link base operation

--- createlink.c
+++ createlink.c
@@ -107,9 +107,41 @@ struct node *delete(struct node *x,struct node *head)
 }  
 
 
+struct node *Insert(struct node *head, int num, struct node *addnode)
+{
+ struct node *p1;  /*p1保存当前需要检查的节点的地址*/ 
+ int n=0;
+ if (head == NULL) /*（结合图示7理解）*/
+ {
+  head = addnode;//it the link is empty
+  addnode->next = NULL;
+  n += 1;
+  return head;
+ } 
+//if link is not empty
+ p1 = head;
+ while (p1->data != num && p1->next != NULL) /*p1指向的节点不是所要查找的，并且它不是最后一个节点，继续往下找*/
+ {  
+  p1 = p1->next; /*后移一个节点*/
+ }
+ 
+ if (num == p1->data) /*找到了（结合图示8理解）*/
+ {//addnode insert after p1
+  addnode->next = p1->next; /*显然node的下一节点是原p1的next*/
+  p1->next = addnode; /*插入后，原p1的下一节点就是要插入的node*/
+  n += 1; /*节点总数增加1个*/
+ }
+ else
+ {
+  printf(" %d not been found! ",n);
+ }
+ 
+ return head;
+}
+
 int main()
 {
-    struct node *head1, *head2;
+    struct node *head1, *head2, *head3;
     int n=0;
     head1=NULL;
     int numnode1=0;
@@ -121,8 +153,11 @@ int main()
     struct node* x = malloc(sizeof(struct node)); 
     x->data=1;
     //int delok=0;
-    
+    struct node* addnode=malloc(sizeof(struct node));
+    addnode->data=9;
     head2=delete(x,head1);
     show(head2);
+    head3=Insert(head2,3,addnode);
+    show(head3);
     return 0;
 }
