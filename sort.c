f2bc11864ff5b42b3003a3ce25eca0c390f2fb04
Bella <906772411@qq.com>
2016-04-29 08:08:50 +0800

    sort ways

--- sort.c
+++ sort.c
@@ -1,5 +1,51 @@
 #include "stdio.h"
 
+void selectsort(int array[],int length)
+{
+    int i,j=0;
+    int temp=0;
+    int index=0;
+    for(i=0;i<length-1;i++)
+    {
+	temp=0;
+	index=i;
+	for(j=i+1;j<length;j++)
+	{
+	    if(array[index]>array[j])
+		index=j;
+	}
+	temp=array[index];
+	array[index]=array[i];	
+	array[i]=temp;	
+    }
+    for(i=0;i<length;i++)
+	printf("%d ",array[i]);
+    printf("\n");
+}
+
+
+void bubblesort(int array[],int length)
+{
+    int i,j=0;
+    int temp=0;
+    for(i=0;i<length-1;i++)
+    {
+	for(j=i;j<length;j++)
+	{
+	    if(array[i]>array[j])
+	    {
+		temp=array[i];		
+		array[j]=array[i];
+		array[i]=temp;
+	    }
+	}
+    }
+    for(i=0;i<length;i++)
+	printf("%d ",array[i]);
+    printf("\n");
+}
+
+
 void insertsort(int array[],int length)
 {
     int i,j=0;
@@ -25,4 +71,6 @@ int main()
 {
     int a[7]={9,4,5,6,7,2,3};
     insertsort(a,7);
+    bubblesort(a,7);
+    selectsort(a,7);
 }
